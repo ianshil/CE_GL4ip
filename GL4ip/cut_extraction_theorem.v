65b943527fcfba05b63ff7e3f33c953b87bb1983
ianshil <73801439+ianshil@users.noreply.github.com>
2022-03-18 17:21:27 +1100

    Update cut_extraction_theorem.v

--- GL4ip/cut_extraction_theorem.v
+++ GL4ip/cut_extraction_theorem.v
@@ -3,14 +3,6 @@ Require Import GL4ip_cut_elim.
 Require Import Extraction.
 Extraction Language Haskell.
 
-(*
-Require Import ExtrHaskellBasic.
-Require Import ExtrHaskellNatInt.
-*)
-
 Unset Extraction Optimize.
 
-(* Time Separate Extraction GL4ip_cut_elimination.*)
-Extraction "/Users/IanShillito/CoqForm/lol.hs" GL4ip_cut_elimination.
-
-Print Assumptions GL4ip_cut_elimination.
+Time Separate Extraction GL4ip_cut_elimination.
